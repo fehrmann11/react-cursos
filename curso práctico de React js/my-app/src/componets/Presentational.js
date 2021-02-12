60b77ed55b008f4a75442c07de0f7359a6074d33
Henry <henry.fehrmann@vasslatam.com>
2021-02-12 14:26:05 -0300

    comienzo aplicación video, configuraciones listas

--- curso práctico de React js/my-app/src/componets/Presentational.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-//Presentacional que no necesiita return
-const Presentacional = () =><h1>Hola Mundo</h1>;
-
-
-export default Presentacional;
---
+++ curso práctico de React js/video/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './components/HelloWorld'
+
+//aplicación completa y donde la voy a empujar, ahora ir a public html
+ReactDOM.render(<HelloWorld/>,document.getElementById("app"));
