97ee77368f687252e3972ac23dcc18ea96da1891
Henry <henry.fehrmann@vasslatam.com>
2021-02-12 14:50:41 -0300

    header

--- curso práctico de React js/video/src/index.js
+++ curso práctico de React js/video/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWorld from './components/HelloWorld'
+import App from './containers/App'
 
 //aplicación completa y donde la voy a empujar, ahora ir a public html
-ReactDOM.render(<HelloWorld/>,document.getElementById("app"));
+ReactDOM.render(<App/>,document.getElementById("app"));
