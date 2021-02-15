0e19abeae1cbaa7ce9e7dee6943883bff0b6c179
Henry <henry.fehrmann@vasslatam.com>
2021-02-15 11:34:05 -0300

    useEffect

--- react hooks/react-hooks/src/App.js
+++ react hooks/react-hooks/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Header from './components/Header'
-
+import Characters from './components/Characters';
 function App() {
   return (
     <div className="App">
       <h1>Hola a todos</h1>
       <Header/>
+      <Characters/>
+      {/*Reto de ponerlo en una tarjeta, con darkmode y css */}
     </div>
   );
 }
---
+++ react hooks/react-hooks/src/components/Characters.jsx
@@ -0,0 +1,58 @@
+import React, {useState,useEffect} from 'react'
+
+//rafce
+
+const Characters = () => {
+
+    const [characters,setCharacters] = useState([]);
+
+    useEffect(()=>{
+        fetch('https://rickandmortyapi.com/api/character/')
+        .then(response=>response.json())
+        .then(data=>setCharacters(data.results));
+    },[])
+
+    return (
+        <div className="Characters">
+            {characters.map(character =>(
+                <h2>{character.name}</h2>
+            ))}
+            
+        </div>
+    )
+}
+
+
+export default Characters;
+
+
+/*
+con async y await
+
+import React, { useState, useEffect } from "react";
+
+const Characters = () => {
+  const [characters, setCharacters] = useState([]);
+
+  const getCharacters = async () => {
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await response.json();
+    setCharacters(data.results);
+  };
+
+  useEffect(() => {
+    getCharacters();
+  }, []);
+
+  return (
+    <div className="Characters">
+      {characters.map((character) => (
+        <h2 key={character.id}>{character.name}</h2>
+      ))}
+    </div>
+  );
+};
+
+export default Characters;
+
+*/
