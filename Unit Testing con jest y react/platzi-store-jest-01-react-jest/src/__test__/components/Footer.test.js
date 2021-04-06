f45669d7d5ab4d62d851468cc0ed84a87600e241
Henry <henry.fehrmann@vasslatam.com>
2021-04-06 17:54:11 -0400

    mocks

--- Unit Testing con jest y react/platzi-store-jest-01-react-jest/src/__test__/components/Footer.test.js
+++ Unit Testing con jest y react/platzi-store-jest-01-react-jest/src/__test__/components/Footer.test.js
@@ -1,10 +1,18 @@
 import React from 'react';
+//me permite montarlo sobre el dom completo y trabajar por los items del componente.
 import { mount } from 'enzyme';
 import Footer from '../../components/Footer';
 
 describe('<Footer />', () => {
+  //paso el componente para que lo monte
+  const footer = mount(<Footer />);
   test('Render del componente Footer', () => {
-    const footer = mount(<Footer />);
     expect(footer.length).toEqual(1);
   });
+  //titulo haga render
+  test('Render del Título', () => {
+    expect(footer.find(".Footer-title").text()).toEqual("Platzi Store");
+  })
 });
+
+//npm test 
